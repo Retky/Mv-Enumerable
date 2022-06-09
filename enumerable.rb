01f5dc311e17c0a77336cc543f1cc02dd655b4a6
fed1k <allamurotovcoder@mail.ru>
2022-06-10 01:26:21 +0500

    Add any and filter methods

--- enumerable.rb
+++ enumerable.rb
@@ -5,4 +5,19 @@ module MyEnumerable
     end
     true
   end
+
+  def any?
+    each do |e|
+      return true if yield(e)
+    end
+    false
+  end
+
+  def filter
+    filtered = []
+    each do |e|
+      filtered.push(e) if yield(e) 
+    end
+    filtered
+  end
 end
